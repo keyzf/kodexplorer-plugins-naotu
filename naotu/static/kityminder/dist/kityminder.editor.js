0ae8280ca0162ee5414c0ae6e36d550780fb359d
paulhybryant <paulhybryant@gmail.com>
2019-02-22 15:53:46 +0800

    Write back to kodexplorer working

--- naotu/static/kityminder/dist/kityminder.editor.js
+++ naotu/static/kityminder/dist/kityminder.editor.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * kityminder-editor - v1.0.67 - 2019-02-21
+ * kityminder-editor - v1.0.67 - 2019-02-22
  * https://github.com/fex-team/kityminder-editor
  * GitHub: https://github.com/fex-team/kityminder-editor 
  * Copyright (c) 2019 ; Licensed 
@@ -4612,20 +4612,19 @@ angular.module('kityminderEditor')
                };
 
                scope.saveMindMap = function() {
-                  fetch(window.appHost+"editor/fileSave", {
-                      "credentials": "include",
-                      "headers": {
-                          "accept": "application/json, text/javascript, */*; q=0.01",
-                          "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6",
-                          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                          "x-requested-with": "XMLHttpRequest"
-                      },
-                      "referrerPolicy": "no-referrer-when-downgrade",
-                      "body": "path=%2Fvar%2Fwww%2Fhtml%2Fdata%2FUser%2Fadmin%2Fhome%2F%E6%88%91%E7%9A%84%E6%96%87%E6%A1%A3%2Ffoobar.txt&charset=utf-8&filestr=MTExMTExMTExMTExMTEx&base64=1",
-                      "method": "POST",
-                      "mode": "cors"
-                  });
-                  console.log(window.filePath);
+                 fetch(window.appHost+"editor/fileSave", {
+                   "credentials": "include",
+                   "headers": {
+                     "accept": "application/json, text/javascript, */*; q=0.01",
+                     "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6",
+                     "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                     "x-requested-with": "XMLHttpRequest"
+                   },
+                   "referrerPolicy": "no-referrer-when-downgrade",
+                   "body": "path="+window.filePath+"&charset=utf-8&filestr="+btoa(unescape(encodeURIComponent(JSON.stringify(window.minder.exportJson()))))+"&base64=1",
+                   "method": "POST",
+                   "mode": "cors"
+                 });
                }
 
                function closeTopTab() {
